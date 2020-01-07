1475fac60ba9a86d465335d97270a38fa292c7de
Jimmy Ngu <jimmynguyc@gmail.com>
2020-01-07 16:26:28 +0800

    Commit

--- config.rb
+++ config.rb
@@ -40,9 +40,10 @@ page '/*.txt', layout: false
 # Build-specific configuration
 # https://middlemanapp.com/advanced/configuration/#environment-specific-settings
 
-# configure :build do
-#   activate :minify_css
-#   activate :minify_javascript
-# end
+configure :build do
+  activate :minify_css
+  activate :minify_javascript
+  config[:host] = "https://jimmynguyc.github.io/builtwithruby"
+end
 
 config[:build_dir] = 'docs'
